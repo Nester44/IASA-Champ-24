9e3bdee73a46c6bb401e0ec6d1481a1bca460c8f
Nester44 <nesterov.mail4@gmail.com>
2024-01-31 21:50:42 +0100

    feat: add chart

---
+++ frontend/src/Home/Chart/Chart.tsx
@@ -0,0 +1,53 @@
+import { WeatherData } from '@/lib/api/fetchWeather'
+import {
+	CartesianGrid,
+	Legend,
+	Line,
+	LineChart,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from 'recharts'
+
+type Props = {
+	dayForecast: WeatherData[]
+}
+
+export const Chart = ({ dayForecast }: Props) => {
+	const mappedData = dayForecast.map((forecast) => ({
+		time: new Date(forecast.timestamp).toLocaleTimeString('uk-ua', {
+			hour: '2-digit',
+			minute: '2-digit',
+		}),
+		temperature: forecast.currentTemperature.toFixed(0),
+	}))
+
+	return (
+		<ResponsiveContainer width='100%' height='100%'>
+			<LineChart
+				width={500}
+				height={600}
+				data={mappedData}
+				margin={{
+					top: 5,
+					right: 30,
+					left: 20,
+					bottom: 5,
+				}}
+			>
+				<CartesianGrid strokeDasharray='3 3' />
+				<XAxis dataKey='time' />
+				<YAxis domain={[-10, 40]} />
+				<Tooltip labelStyle={{ color: 'black' }} />
+				<Legend />
+				<Line
+					type='monotone'
+					dataKey='temperature'
+					stroke='#8884d8'
+					activeDot={{ r: 8 }}
+				/>
+			</LineChart>
+		</ResponsiveContainer>
+	)
+}
--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,9 +1,9 @@
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 import { fetchWeather } from '@/lib/api/fetchWeather'
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
+import { Cards } from './Cards'
+import { Chart } from './Chart'
 import { SelectCity } from './SelectCity'
-import WeatherCard from './WeatherCard'
 
 export function Home() {
 	const [location, setLocation] = useState('')
@@ -14,36 +14,18 @@ export function Home() {
 		enabled: Boolean(location),
 	})
 
-	if (isSuccess) {
-		console.log(data)
-	}
-
 	return (
 		<div className='dark px-12'>
-			<div className='max-h-12 relative'>
+			<div className='max-h-11 z-10 relative'>
 				<SelectCity setLocation={setLocation} />
 			</div>
 			<div>
-				{location && <p className='text-lg'>{location}</p>}
-
 				{isSuccess && data?.length > 0 && (
 					<>
-						<img
-							src='https://static.vecteezy.com/system/resources/previews/000/406/151/original/vector-illustration-of-data-analysis-graph.jpg'
-							alt=''
-							className='w-96 h-96 mx-auto my-16'
-						/>
-						<ScrollArea className=' whitespace-nowrap rounded-md border'>
-							<div className='flex w-max space-x-4 p-4'>
-								{data?.map((forecast) => (
-									<WeatherCard
-										key={forecast.timestamp}
-										forecast={forecast}
-									/>
-								))}
-							</div>
-							<ScrollBar orientation='horizontal' />
-						</ScrollArea>
+						<div className='mx-auto my-8' style={{ height: 450 }}>
+							<Chart dayForecast={data} />
+						</div>
+						<Cards dayForecast={data} />
 					</>
 				)}
 			</div>
