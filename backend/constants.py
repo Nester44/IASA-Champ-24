d7b22cdbc49069803c03b8c2cbccd21954b999e1
Nester44 <nesterov.mail4@gmail.com>
2024-02-02 21:29:17 +0100

    remove cities print

--- backend/constants.py
+++ backend/constants.py
@@ -7,6 +7,4 @@ indicators = ["temperature_2m", "relative_humidity_2m", "precipitation"]
 
 cities = list(map(str.lower, os.getenv("CITIES").split(", ")))
 
-print(cities)
-
 dataset_last_info = "2024.01.31"
